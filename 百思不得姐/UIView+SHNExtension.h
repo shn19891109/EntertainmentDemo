09b3955f9cc5436f850035d92948455cbf69b3be
shn <1058216614@qq.com>
2016-02-24 21:07:47 +0800

    添加控件frame分类

---
+++ 百思不得姐/UIView+SHNExtension.h
@@ -0,0 +1,26 @@
+//
+//  UIView+SHNExtension.h
+//  百思不得姐
+//
+//  Created by 苏浩楠 on 16/2/24.
+//  Copyright © 2016年 suhaonan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface UIView (SHNExtension)
+
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+
+
+/** 在分类中声明@property, 只会生成方法的声明, 不会生成方法的实现和带有_下划线的成员变量*/
+//只会声明set与get方法
+//- (CGFloat)x;
+//- (void)setX:(CGFloat)x;
+
+@end
---
+++ 百思不得姐/UIView+SHNExtension.m
@@ -0,0 +1,60 @@
+//
+//  UIView+SHNExtension.m
+//  百思不得姐
+//
+//  Created by 苏浩楠 on 16/2/24.
+//  Copyright © 2016年 suhaonan. All rights reserved.
+//
+
+#import "UIView+SHNExtension.h"
+
+@implementation UIView (SHNExtension)
+- (void)setWidth:(CGFloat)width
+{
+    CGRect frame = self.frame;
+    frame.size.width = width;
+    self.frame = frame;
+}
+
+- (void)setHeight:(CGFloat)height
+{
+    CGRect frame = self.frame;
+    frame.size.height = height;
+    self.frame = frame;
+}
+
+- (void)setX:(CGFloat)x
+{
+    CGRect frame = self.frame;
+    frame.origin.x = x;
+    self.frame = frame;
+}
+
+- (void)setY:(CGFloat)y
+{
+    CGRect frame = self.frame;
+    frame.origin.y = y;
+    self.frame = frame;
+}
+
+- (CGFloat)width
+{
+    return self.frame.size.width;
+}
+
+- (CGFloat)height
+{
+    return self.frame.size.height;
+}
+
+- (CGFloat)x
+{
+    return self.frame.origin.x;
+}
+
+- (CGFloat)y
+{
+    return self.frame.origin.y;
+}
+
+@end
